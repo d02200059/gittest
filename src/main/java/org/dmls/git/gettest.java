27497814cb2bfbdc8de9667150ae3a1adef34a70
dmls <327572070@qq.com>
2022-09-27 16:10:43 +0800

    0927 commit

---
+++ src/main/java/org/dmls/git/gettest.java
@@ -0,0 +1,21 @@
+package org.dmls.git;
+
+/**
+ * ClassName: gettest
+ * Description:
+ * date: 2022/9/27 16:00
+ *
+ * @author 段梦离殇
+ * @since JDK 1.8
+ */
+public class gettest {
+
+    public static void main(String[] args) {
+
+        System.out.println("2022/09/27");
+
+
+
+    }
+
+}
